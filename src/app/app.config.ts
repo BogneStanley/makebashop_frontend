9c5c02975b3390f3573ec64d2514629a9147dd77
Bogne Stanley <bogne.stanley@gmail.com>
2026-06-22 23:17:22 +0100

    ui refacto and assets files added

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -6,14 +6,34 @@ import { providePrimeNG } from 'primeng/config';
 import Aura from '@primeuix/themes/aura';
 import localeFr from '@angular/common/locales/fr';
 import { registerLocaleData } from '@angular/common';
+import { definePreset } from '@primeuix/themes';
 
 registerLocaleData(localeFr);
+
+const MyPreset = definePreset(Aura, {
+  semantic: {
+    primary: {
+      50: '{orange.50}',
+      100: '{orange.100}',
+      200: '{orange.200}',
+      300: '{orange.300}',
+      400: '{orange.400}',
+      500: '{orange.500}',
+      600: '{orange.600}',
+      700: '{orange.700}',
+      800: '{orange.800}',
+      900: '{orange.900}',
+      950: '{orange.950}',
+    },
+  },
+});
+
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     providePrimeNG({
       theme: {
-        preset: Aura,
+        preset: MyPreset,
         options: {
           darkModeSelector: false, // or 'none'
         },
